9af72742143acb743c5d624bbac0e608622731b6
Francisco Nogales <fco.nogalesr@gmail.com>
2022-03-20 02:40:11 +0000

    worked with RWMutex

--- avanzado/sync/main.go
+++ avanzado/sync/main.go
@@ -10,7 +10,7 @@ var (
 	balance int = 100
 )
 
-func Deposit(amount int, wg *sync.WaitGroup, lock *sync.Mutex) {
+func Deposit(amount int, wg *sync.WaitGroup, lock *sync.RWMutex) {
 	defer wg.Done()
 	lock.Lock() //se bloquea el programa y tiene que esperar hasta que el que tiene lock lo desbloquee. La siguiente goroutine tiene que esperar a que
 	// este lock llegue a unlock -(condicion de carrera)
@@ -19,19 +19,21 @@ func Deposit(amount int, wg *sync.WaitGroup, lock *sync.Mutex) {
 	lock.Unlock()
 }
 
-func Balance() int {
+func Balance(lock *sync.RWMutex) int {
+	lock.RLock()
 	b := balance
+	lock.RUnlock()
 	return b
 }
 
 func main() {
 	var wg sync.WaitGroup //para bloquear el programa
-	var lock sync.Mutex   //para evitar que el deposito este usando la misma variable en diferentes goroutnes
+	var lock sync.RWMutex //para evitar que el deposito este usando la misma variable en diferentes goroutnes
 	for i := 0; i <= 5; i++ {
 		wg.Add(1)
 		go Deposit(i*100, &wg, &lock)
 	}
 	wg.Wait()
-	fmt.Println(Balance())
+	fmt.Println(Balance(&lock))
 
 }
